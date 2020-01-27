4b2f1f6b42252ff5eace41f5fa897240bc91fd23
mickael grand <mickael.grand.simplon@gmail.com>
2020-01-27 15:41:59 +0100

    misea jour composer

--- src/Migrations/Version20200127144056.php
+++ src/Migrations/Version20200127144056.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200127134530 extends AbstractMigration
+final class Version20200127144056 extends AbstractMigration
 {
     public function getDescription() : string
     {
